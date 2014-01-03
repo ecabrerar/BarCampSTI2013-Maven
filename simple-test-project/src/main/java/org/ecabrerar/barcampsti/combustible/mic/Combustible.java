b3fa20259cdefd3eba3cdd03d310f248d143c31b
Eudris Cabrera Rodriguez <eudris@gmail.com>
2014-01-03 01:50:31 -0400

    agregando archivos resources y clases java

---
+++ simple-test-project/src/main/java/org/ecabrerar/barcampsti/combustible/mic/Combustible.java
@@ -0,0 +1,94 @@
+/**
+ * 
+ */
+/**
+ * @author ecabrerar
+ *
+ */
+package org.ecabrerar.barcampsti.combustible.mic;
+
+
+public class Combustible {
+
+	private String gasolinaPremium;
+	private String gasolinaRegular;
+	private String gasoilPremium;
+	private String gasoilRegular;
+	private String kerosene;
+	private String glp;
+	private String gnv;
+	
+	
+	public Combustible() {}
+
+
+	public String getGasolinaPremium() {
+		return gasolinaPremium;
+	}
+
+
+	public void setGasolinaPremium(String gasolinaPremium) {
+		this.gasolinaPremium = gasolinaPremium;
+	}
+
+
+	public String getGasolinaRegular() {
+		return gasolinaRegular;
+	}
+
+
+	public void setGasolinaRegular(String gasolinaRegular) {
+		this.gasolinaRegular = gasolinaRegular;
+	}
+
+
+	public String getGasoilPremium() {
+		return gasoilPremium;
+	}
+
+
+	public void setGasoilPremium(String gasoilPremium) {
+		this.gasoilPremium = gasoilPremium;
+	}
+
+
+	public String getGasoilRegular() {
+		return gasoilRegular;
+	}
+
+
+	public void setGasoilRegular(String gasoilRegular) {
+		this.gasoilRegular = gasoilRegular;
+	}
+
+
+	public String getKerosene() {
+		return kerosene;
+	}
+
+
+	public void setKerosene(String kerosene) {
+		this.kerosene = kerosene;
+	}
+
+
+	public String getGlp() {
+		return glp;
+	}
+
+
+	public void setGlp(String glp) {
+		this.glp = glp;
+	}
+
+
+	public String getGnv() {
+		return gnv;
+	}
+
+
+	public void setGnv(String gnv) {
+		this.gnv = gnv;
+	}
+	
+}
